a142ec9fbe225ec47b49eb83bd9a4c9af791573b
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-01-30 17:49:13 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long num1 = sc.nextLong();
+		long num2 = sc.nextLong();
+		long num3 = sc.nextLong();
+		long sum = num1 + num2 + num3;
+		System.out.println(sum);
+	}
+}
