d4b3b5fbb28bbb58e911221a141c0c8e475330d2
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-02-12 20:11:17 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		
+		int a = B / 100;
+		int b = (B - (a * 100)) / 10;
+		int c = B - (a * 100) - (b * 10);
+		
+		System.out.println(A * c);
+		System.out.println(A * b);
+		System.out.println(A * a);
+		System.out.println(A * B);
+	}
+}
