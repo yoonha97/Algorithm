8fdb5b005ed062626e04f3c5602c6f846a514577
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-03-03 15:13:36 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 104 ms, Memory: 17,424 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,15 +1,9 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int[][] arr;
-    static int N, K;
-    static int[] dr = { 1, 0 };
-    static int[] dc = { 0, 1 };
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
@@ -18,9 +12,9 @@ public class Solution {
 
         for (int t = 1; t <= T; t++) {
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            arr = new int[N][N];
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            int[][]arr = new int[N][N];
 
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
