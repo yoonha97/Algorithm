3229ccdd0cacd0372ae94754114f7b55fdce60c6
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-03-20 17:33:09 +0900

    [Silver I] Title: 안전 영역, Time: 244 ms, Memory: 19104 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,61 +1,58 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static int[] dr = { 0, 1, 0, -1 };
+	static int[] dc = { 1, 0, -1, 0 };
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int[][] arr = new int[N][N];
-		int[] dr = { 0, 1, 0, -1 };
-		int[] dc = { 1, 0, -1, 0 };
 		int maxH = 0;
-		
-		for(int i = 0; i < N; i++) {
+
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for(int j = 0; j < N; j++) {
+			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 				maxH = Math.max(maxH, arr[i][j]);
 			}
 		}
 //		System.out.println(Arrays.deepToString(arr));
 //		System.out.println(maxH);
-		
+
 		int maxNum = 0;
-		for(int setH = 0; setH < maxH; setH++) {
+		for (int setH = 0; setH < maxH; setH++) {
 			int cnt = 0;
-			Queue<int[]> queue = new LinkedList<>();
+			Stack<int[]> stack = new Stack<>();
 			boolean[][] visited = new boolean[N][N];
-			
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < N; j++) {
-					if(arr[i][j] > setH && !visited[i][j]) {
-						visited[i][j] = true;
+
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					if (arr[i][j] > setH && !visited[i][j]) {
+						dfs(arr, visited, i, j, setH, N);
 						cnt++;
-						
-						queue.offer(new int[] {i, j});
-						
-						while(!queue.isEmpty()) {
-							int[] pos = queue.poll();
-							
-							for(int dir = 0; dir < 4; dir++) {
-								int nr = pos[0] + dr[dir];
-								int nc = pos[1] + dc[dir];
-								
-								if(nr >= 0 && nr < N && nc >= 0 && nc < N && arr[nr][nc] > setH && !visited[nr][nc]) {
-									visited[nr][nc] = true;
-									queue.offer(new int[] {nr, nc});
-								}
-							}
-						}
 					}
 				}
 			}
 			maxNum = Math.max(maxNum, cnt);
 		}
 		System.out.println(maxNum);
+	}
+
+	public static void dfs(int[][] arr, boolean[][] visited, int r, int c, int h, int N) {
+		visited[r][c] = true;
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nr = r + dr[dir];
+			int nc = c + dc[dir];
+
+			if (nr >= 0 && nr < N && nc >= 0 && nc < N && arr[nr][nc] > h && !visited[nr][nc]) {
+				dfs(arr, visited, nr, nc, h, N);
+			}
+		}
 	}
 }
