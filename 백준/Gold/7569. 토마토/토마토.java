8ade0230b644e661523f11059a40d7e3e1a95727
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-03-20 19:03:28 +0900

    [Gold V] Title: 토마토, Time: 676 ms, Memory: 127904 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -6,16 +6,21 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int M, N, H;
+	static int[][][] arr;
+	static boolean[][][] visited;
+	static int day, cnt;
+	static int[] dr = { -1, 0, 1, 0, 0, 0 };
+	static int[] dc = { 0, 1, 0, -1, 0, 0 };
+	static int[] dh = { 0, 0, 0, 0, 1, -1 };
+	
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        int H = Integer.parseInt(st.nextToken());
-        int[][][] arr = new int[N][M][H];
-        int[] dr = { -1, 0, 1, 0, 0, 0 };
-        int[] dc = { 0, 1, 0, -1, 0, 0 };
-        int[] dh = { 0, 0, 0, 0, 1, -1 };
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        arr = new int[N][M][H];
         
         for(int h = 0; h < H; h++) { // 높이
             for(int i = 0; i < N; i++) {
@@ -38,52 +43,56 @@ public class Main {
 //            System.out.println();
 //        }
         
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][][] visited = new boolean[N][M][H];
-        int day = -1; // 다 익는데 걸리는 시간, while문에 들어갈때 +1 되기 때문에 -1로 초기화
-        int cnt = 0; // 안익은 토마토 수
-        
-        for(int h = 0; h < H; h++) {
-            for(int i = 0; i < N; i++) {
-                for(int j = 0; j < M; j++) {
-                    if(arr[i][j][h] == 1 && !visited[i][j][h]) {
-                    	visited[i][j][h] = true;
-                    	q.offer(new int[] {i, j, h});
-                    } else if(arr[i][j][h] == 0) {
-                    	cnt++;
-                    }
-                }
-            }
-        }
-        
-        while(!q.isEmpty()) {
-        	int size = q.size();
-        	for(int i = 0; i < size; i++) {
-        		int[] pos = q.poll();
-        		int r = pos[0];
-        		int c = pos[1];
-        		int h = pos[2];
-        		
-        		for(int dir = 0; dir < 6; dir++) {
-        			int nr = r + dr[dir];
-        			int nc = c + dc[dir];
-        			int nh = h + dh[dir];
-        			
-        			if(nr >= 0 && nr < N && nc >= 0 && nc < M && nh >= 0 && nh < H && arr[nr][nc][nh] == 0 && !visited[nr][nc][nh]) {
-        				visited[nr][nc][nh] = true;
-        				arr[nr][nc][nh] = 1;
-        				q.offer(new int[] {nr, nc, nh});
-        				cnt--;
-        			}
-        		}
-        	}
-        	day++;
-        }
+        bfs(arr, visited, M, N, H);
         
         if(cnt == 0) {
         	System.out.println(day);
         } else {
         	System.out.println(-1);
         }
+    }
+    
+    static void bfs(int[][][] arr, boolean[][][] visited, int M, int N, int H) {    	
+    	Queue<int[]> q = new LinkedList<>();
+    	visited = new boolean[N][M][H];
+    	day = -1; // 다 익는데 걸리는 시간, while문에 들어갈때 +1 되기 때문에 -1로 초기화
+    	cnt = 0; // 안익은 토마토 수
+    	
+    	for(int h = 0; h < H; h++) {
+    		for(int i = 0; i < N; i++) {
+    			for(int j = 0; j < M; j++) {
+    				if(arr[i][j][h] == 1 && !visited[i][j][h]) {
+    					visited[i][j][h] = true;
+    					q.offer(new int[] {i, j, h});
+    				} else if(arr[i][j][h] == 0) {
+    					cnt++;
+    				}
+    			}
+    		}
+    	}
+    	
+    	while(!q.isEmpty()) {
+    		int size = q.size();
+    		for(int i = 0; i < size; i++) {
+    			int[] pos = q.poll();
+    			int r = pos[0];
+    			int c = pos[1];
+    			int h = pos[2];
+    			
+    			for(int dir = 0; dir < 6; dir++) {
+    				int nr = r + dr[dir];
+    				int nc = c + dc[dir];
+    				int nh = h + dh[dir];
+    				
+    				if(nr >= 0 && nr < N && nc >= 0 && nc < M && nh >= 0 && nh < H && arr[nr][nc][nh] == 0 && !visited[nr][nc][nh]) {
+    					visited[nr][nc][nh] = true;
+    					arr[nr][nc][nh] = 1;
+    					q.offer(new int[] {nr, nc, nh});
+    					cnt--;
+    				}
+    			}
+    		}
+    		day++;
+    	}
     }
 }
