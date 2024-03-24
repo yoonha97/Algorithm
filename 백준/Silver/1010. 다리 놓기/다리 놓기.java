57876964bd0dc4d3ecac9cba5e6ab2b6f0623638
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-03-24 22:20:24 +0900

    [Silver V] Title: 다리 놓기, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 0; tc < T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			System.out.println(comb(y,x));
+		}
+	}
+	
+    static long comb(int n, int r) {
+        long result = 1;
+        r = Math.min(r, n - r);
+        for (int i = 0; i < r; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
+    }
+}
