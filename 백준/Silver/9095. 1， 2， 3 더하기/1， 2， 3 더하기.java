243ab09c134240bb8415508bccf3047790697824
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-06-17 16:37:12 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 200 ms, Memory: 17624 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
+		
+		for(int i = 4; i < 11; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+		}
+		
+		for(int tc = 0; tc < T; tc++) {
+			System.out.println(dp[sc.nextInt()]);
+		}
+	}
+}
