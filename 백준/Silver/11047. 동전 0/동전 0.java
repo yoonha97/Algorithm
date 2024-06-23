ed8fea08ffe30f4c6e12262322f50d43f110134a
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-06-23 11:11:13 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,22 +1,27 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int k = sc.nextInt();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		int[] coin = new int[N];
+		int cnt = 0;
 		
-		int[] arr = new int[n];
-		for(int i=0;i<n;i++) {
-			arr[i]=sc.nextInt();
+		for(int i = 0; i < N; i++) {
+			coin[i] = Integer.parseInt(br.readLine());
 		}
-		Arrays.sort(arr);
-		int cnt=0;
-		for(int i=n-1;i>=0;i--) {
-			cnt+=k/arr[i];
-			k%=arr[i];
+		
+		for(int i = N - 1; i >= 0; i--) {
+			if(K >= coin[i]) {
+				cnt += K / coin[i];
+				K = K % coin[i];
+			}
 		}
+		
 		System.out.println(cnt);
 	}
 }
