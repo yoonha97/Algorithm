8055c8e7e6559cb30dbcfe96c9c2d432a7af2b4c
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-07-05 16:12:56 +0900

    [Bronze IV] Title: 입실 관리, Time: 132 ms, Memory: 14076 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		for(int i = 0; i < N; i++){
+			String str = br. readLine();
+			System.out.println(str.toLowerCase());
+		}
+	}	
+}
