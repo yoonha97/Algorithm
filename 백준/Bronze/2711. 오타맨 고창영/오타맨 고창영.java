8953361d09083f523b4d39f994e92f18ff974ce4
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2024-07-05 16:38:11 +0900

    [Bronze II] Title: 오타맨 고창영, Time: 128 ms, Memory: 14156 KB -BaekjoonHub

---
+++ 백준/Bronze/2711. 오타맨 고창영/오타맨 고창영.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for(int tc = 0; tc < T; tc++){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken()) - 1;
+			String str = st.nextToken();
+			StringBuilder sb = new StringBuilder();
+			sb.append(str);
+			sb.deleteCharAt(i);
+			System.out.println(sb);
+		}
+	}
+}
