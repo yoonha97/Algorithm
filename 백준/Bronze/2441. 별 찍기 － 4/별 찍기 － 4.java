09a8371ad6c9e8fc4efcb276c5d26df1468496ae
yoonha97 <155246172+yoonha97@users.noreply.github.com>
2025-02-11 09:39:57 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 116 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -2,18 +2,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		for(int i = 0; i < N; i++){
-			for(int j = 0; j < i; j++){
-				System.out.print(' ');
-			}
-			for(int j = 0; j < N - i; j++){
-				System.out.print('*');
-			}
-			System.out.println();
-		}
-	}
+        for (int i = 0; i < N; i++) {
+            System.out.println(" ".repeat(i) + "*".repeat(N - i));
+        }
+    }
 }
